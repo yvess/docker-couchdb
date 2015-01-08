ba83d0bec37dbb3a9430a5a4eeea4af541c6ae85
Yves Serrano <y@yas.ch>
2015-01-08 23:44:47 +0100

    inject local.append.ini from child container, fix run problem, cd to working dir

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,18 +11,30 @@ if [ "$1" = 'couchdb' ]; then
   if [ ! -d "${COUCHDB_DATA}" ] || [ ! -d "${COUCHDB_LOG}" ]; then
     echo "creating service directories"
     mkdir -p "$COUCHDB_DATA" "$COUCHDB_LOG" /var/run/couchdb
-    chown -R couchdb:couchdb "$COUCHDB_DATA" "$COUCHDB_LOG" /var/run/couchdb /etc/couchdb/local.ini
+    chown -R couchdb:couchdb "$COUCHDB_DATA" "$COUCHDB_LOG" \
+      /var/run/couchdb /etc/couchdb/local.ini
+    if find /etc/couchdb/local.d/* -maxdepth 0 -type f; then
+      chown -R couchdb:couchdb /etc/couchdb/local.d/*
+    fi
   fi
   # set env vars in local.ini
   if grep -qv -e "-COUCHDB_DATA-" /etc/couchdb/local.ini; then
+    echo "update local.ini"
     # escape / in path for sed
     COUCHDB_DATA_ESC=${COUCHDB_DATA//\//\\/}
     COUCHDB_LOG_ESC=${COUCHDB_LOG//\//\\/}
-    sed -i -e "s/-COUCHDB_DATA-/${COUCHDB_DATA_ESC}/" -e "s/-COUCHDB_LOG-/${COUCHDB_LOG_ESC}/" /etc/couchdb/local.ini
-    sed -i -e "s/-COUCHDB_ADMIN-/${COUCHDB_ADMIN}/" -e "s/-COUCHDB_ADMINPASS-/${COUCHDB_ADMINPASS}/" /etc/couchdb/local.ini
+    sed -i -e "s/-COUCHDB_DATA-/${COUCHDB_DATA_ESC}/" \
+      -e "s/-COUCHDB_LOG-/${COUCHDB_LOG_ESC}/" /etc/couchdb/local.ini
+    sed -i -e "s/-COUCHDB_ADMIN-/${COUCHDB_ADMIN}/" \
+      -e "s/-COUCHDB_ADMINPASS-/${COUCHDB_ADMINPASS}/" /etc/couchdb/local.ini
+  fi
+  # append config from child container
+  if [ -f "/tmp/local.append.ini" ]; then
+    echo "append local.append.ini"
+    cat /tmp/local.append.ini >> /etc/couchdb/local.ini && rm /tmp/local.append.ini
   fi
   cd "$COUCHDB_DATA"
-  HOME="$COUCHDB_DATA" exec su couchdb -c /usr/bin/couchdb couchdb "$@"
+  HOME="$COUCHDB_DATA" su couchdb -c /usr/bin/couchdb couchdb
 fi
 
 exec "$@"
