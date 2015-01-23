f384a57cbcd3faaef825f66ccbce96ce33d52f5e
Yves Serrano <y@yas.ch>
2015-01-23 11:16:03 +0100

    added entrypoint-couchdb.sh, removed not needed _ESC suffix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,32 +2,44 @@
 set -e
 
 if [ "$1" = 'couchdb' ]; then
-  # set default values if not set
+  # SET DEFAULT VALUES IF NOT SET
   COUCHDB_DATA=${COUCHDB_DATA:-/var/services/data/couchdb}
   COUCHDB_LOG=${COUCHDB_LOG:-/var/services/log/couchdb/couchdb.log}
   COUCHDB_ADMIN=${COUCHDB_ADMIN:-admin}
   COUCHDB_ADMINPASS=${COUCHDB_ADMINPASS:-admin}
-  # make dirs run / etc / couchdb
+
+  # MAKE DIRS RUN / ETC / COUCHDB
   mkdir -p /var/run/couchdb "$COUCHDB_DATA" "$COUCHDB_LOG"
 
-  # set env vars in local.ini
+  # SET ENV VARS IN LOCAL.INI
   if grep -qv -e "-COUCHDB_DATA-" /etc/couchdb/local.ini; then
     echo "update local.ini"
-    sed -i -e "s#-COUCHDB_DATA-#${COUCHDB_DATA_ESC}#" \
-      -e "s#-COUCHDB_LOG-#${COUCHDB_LOG_ESC}#" /etc/couchdb/local.ini
+    sed -i -e "s#-COUCHDB_DATA-#${COUCHDB_DATA}#" \
+      -e "s#-COUCHDB_LOG-#${COUCHDB_LOG}#" /etc/couchdb/local.ini
     sed -i -e "s#-COUCHDB_ADMIN-#${COUCHDB_ADMIN}#" \
       -e "s#-COUCHDB_ADMINPASS-#${COUCHDB_ADMINPASS}#" /etc/couchdb/local.ini
   fi
-  # append config from child container
+
+  # APPEND CONFIG FROM CHILD CONTAINER
   if [ -f "/tmp/local.append.ini" ]; then
     echo "append local.append.ini"
     cat /tmp/local.append.ini >> /etc/couchdb/local.ini && rm /tmp/local.append.ini
   fi
-  # make sure everything is owned by couchdb
+
+  # RUN COUCHDB ENTRY SCRIPT
+  if [ -f "/entrypoint-couchdb.sh" ]; then
+    /entrypoint-couchdb.sh
+  fi
+
+  # MAKE SURE EVERYTHING IS OWNED BY COUCHDB
   chown -R couchdb:couchdb "$COUCHDB_DATA" "$COUCHDB_LOG" /var/run/couchdb /etc/couchdb/local.ini
-  if find /etc/couchdb/local.d/* -maxdepth 0 -type f; then
+  if [ -f /etc/couchdb/local.d/* ]; then
     chown -R couchdb:couchdb /etc/couchdb/local.d/*
   fi
+
+  # CLEANUP
+  rm -Rf /root/build
+
   cd "$COUCHDB_DATA"
   HOME="$COUCHDB_DATA" su couchdb -c /usr/bin/couchdb couchdb
 fi
